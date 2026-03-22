0e3180ed96c9e4e77bbef6c8d21791bd4c789f3a
ondaonde <am.zariifa@gmail.com>
2026-03-22 18:23:30 +0700

    add biodata anggota and styling

--- main/views.py
+++ main/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
 
-def show_main(request):
+def show_anggota(request):
+    anggota = [
+        {
+            "nama": "Farras Syafiq U",
+            "npm": "2406495722",
+            "jenis_kelamin": "Laki-laki"
+        },
+        {
+            "nama": "Neal Guarddin",
+            "npm": "2406348282",
+            "jenis_kelamin": "Laki-laki"
+        },
+        {
+            "nama": "Muhammad Fazil Tirtana",
+            "npm": "2306274983",
+            "jenis_kelamin": "Laki-laki"
+        },
+        {
+            "nama": "Izzudin Abdul Rasyid",
+            "npm": "2406495786",
+            "jenis_kelamin": "Laki-laki"
+        },
+        {
+            "nama": "Najwa Zarifa",
+            "npm": "2306207796",
+            "jenis_kelamin": "Perempuan"
+        }
+    ]
+
+    font = request.GET.get('font', 'font-sans')
+    warna = request.GET.get('warna', '#000000')
+
     context = {
-        'npm' : '2306207796',
-        'nama': 'Najwa Zarifa',
-        'jenis_kelamin': 'Perempuan'
+        'anggota': anggota,
+        'font': font,
+        'warna': warna
     }
 
     return render(request, "main.html", context)
--- pkpl_tk2/urls.py
+++ pkpl_tk2/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main.views import show_main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
